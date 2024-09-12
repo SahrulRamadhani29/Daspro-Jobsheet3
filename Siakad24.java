cdbda68686599a1b81b0f413e7b94e60532d1e00
SyahrulRamadhani29 <Ramabusiness2910@gmail.com>
2024-09-12 07:14:35 +0700

    menambahkan file jobsheet

---
+++ Siakad24.java
@@ -0,0 +1,18 @@
+/**
+ * main
+ */
+public class ContohVariable24 {
+public static void main(String[] args) {
+    String  salahSatuHobySayaAdalah = "Renang";
+    boolean  isPandai = true;
+    char jenisKelamin = 'L';
+    byte _umurSayaSekarang = 19;
+    double $ipk =  4.0, tinggi =1.74;
+    System.out.println("Apakah pandai?" + isPandai);
+    System.out.println("Jenis kelamin:"+ jenisKelamin);
+    System.out.println("Umurku saat ini:" + _umurSayaSekarang);
+    System.out.println(String.format("saya  beripk %s, dengan tinggi badan %s", $ipkSaya, tinggiSaya ));
+    
+}
+    
+}
